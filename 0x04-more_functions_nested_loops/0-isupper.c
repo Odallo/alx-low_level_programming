a078e443bfe9fdb351f3547ffb9e8155a7527021
Odallo <eugeneodali181@gmail.com>
2023-10-13 01:28:08 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,14 @@
 /**
  * int _isupper - to check for uppercase letters
  *
- * return: 0
+ * return: 1 if uppercase and 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
 }
