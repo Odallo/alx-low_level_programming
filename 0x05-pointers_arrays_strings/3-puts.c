1f5d3c21b6bc663ad7daf0e67d5798ee3eff8082
Odallo <eugeneodali181@gmail.com>
2023-10-17 12:24:43 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,16 +4,16 @@
  * @str: string to print
  *
  * Description: prints a string
- * On success: returns no error
+ * On success: return 0
  */
 void _puts(char *str)
 {
-	int i = 0;
+        int i = 0;
 
-	while (*(str + i) != '\0')
-	{
-		putchar(*(str + i));
-		i++;
-	}
-	putchar('\n');
+        while (*(str + i) != '\0')
+        {
+        	putchar(*(str + i));
+                i++;
+        }
+        putchar(10);
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+*print_rev - Prints a string in reverse order
+*@s: String to reverse
+*Return: Nothing
+*/
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
